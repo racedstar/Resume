a9014bdf7de9e1b36030d148b7fbd73a438d145d
racedstar <racedstar@gmail.com>
2019-01-28 01:17:51 +0800

    Updates

---
+++ precache-manifest.b8440b3afedc6bb83694575731e0f4ad.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d5efb982c82b6672434",
+    "url": "/Resume/static/js/runtime~main.8d5efb98.js"
+  },
+  {
+    "revision": "3d40c9c4bdbad38ab29b",
+    "url": "/Resume/static/js/main.3d40c9c4.chunk.js"
+  },
+  {
+    "revision": "76efa60d2c7dd3732acc",
+    "url": "/Resume/static/js/1.76efa60d.chunk.js"
+  },
+  {
+    "revision": "3d40c9c4bdbad38ab29b",
+    "url": "/Resume/static/css/main.98220c6d.chunk.css"
+  },
+  {
+    "revision": "c9c8ecb10062bb2e049815b583e18182",
+    "url": "/Resume/index.html"
+  }
+];
